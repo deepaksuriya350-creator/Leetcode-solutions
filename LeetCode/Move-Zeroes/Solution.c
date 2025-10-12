37821f2805c81286c55a5026741b2f7cc543e8db
deepaksuriya350-creator <deepaksuriya350@gmail.com>
2025-10-12 13:13:11 +0530

    Add solution for Move Zeroes

---
+++ LeetCode/Move-Zeroes/Solution.c
@@ -0,0 +1,12 @@
+void moveZeroes(int* nums, int numsSize) {
+    int lastNonZeroFoundAt = 0; 
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != 0) {
+            nums[lastNonZeroFoundAt] = nums[i];
+            lastNonZeroFoundAt++;
+        }
+    }
+    for (int i = lastNonZeroFoundAt; i < numsSize; i++) {
+        nums[i] = 0;
+    }
+}
